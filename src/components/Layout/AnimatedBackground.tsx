174c022d39216fe80a0f24795f23b78e256fccc6
Karia-Ab <kariaofficials8@gmail.com>
2026-07-29 01:03:33 +0330

    Footer Done

--- src/components/Layout/AnimatedBackground.tsx
+++ src/components/Layout/AnimatedBackground.tsx
@@ -3,43 +3,45 @@ import { motion } from "framer-motion";
 export default function AnimatedBackground() {
   return (
     <>
-      <motion.div
-        animate={{
-          x: [0, 60, -20, 0],
-          y: [0, -40, 30, 0],
-        }}
-        transition={{
-          duration: 18,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-        className="absolute -left-40 top-20 h-96 w-96 rounded-full bg-[#C47A2C]/10 blur-[140px]"
-      />
+      <div className="pointer-events-none absolute inset-0 overflow-hidden">
+        <motion.div
+          animate={{
+            x: [0, 60, -20, 0],
+            y: [0, -40, 30, 0],
+          }}
+          transition={{
+            duration: 18,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+          className="absolute -left-40 top-20 h-96 w-96 rounded-full bg-[#C47A2C]/10 blur-[140px]"
+        />
 
-      <motion.div
-        animate={{
-          x: [0, -70, 30, 0],
-          y: [0, 50, -20, 0],
-        }}
-        transition={{
-          duration: 22,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-        className="absolute -right-32 bottom-10 h-[34rem] w-[34rem] rounded-full bg-[#8B5A3C]/10 blur-[170px]"
-      />
+        <motion.div
+          animate={{
+            x: [0, -70, 30, 0],
+            y: [0, 50, -20, 0],
+          }}
+          transition={{
+            duration: 22,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+          className="absolute -right-32 bottom-10 h-[34rem] w-[34rem] rounded-full bg-[#8B5A3C]/10 blur-[170px]"
+        />
 
-      <motion.div
-        animate={{
-          scale: [1, 1.15, 1],
-        }}
-        transition={{
-          duration: 12,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-        className="absolute left-1/2 top-1/2 h-80 w-80 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#E59B1E]/5 blur-[120px]"
-      />
+        <motion.div
+          animate={{
+            scale: [1, 1.15, 1],
+          }}
+          transition={{
+            duration: 12,
+            repeat: Infinity,
+            ease: "easeInOut",
+          }}
+          className="absolute left-1/2 top-1/2 h-80 w-80 -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#E59B1E]/5 blur-[120px]"
+        />
+      </div>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,18 @@
-import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import AnimatedBackground from "../components/Layout/AnimatedBackground";
 
 export default function Home() {
   return (
     <>
-      <AnimatedBackground />
-      <Navbar />
-      <main></main>
+      <div className="min-h-screen flex flex-col">
+        <AnimatedBackground />
+        <Navbar />
+
+        <main className="flex-1">...</main>
+
+        <Footer />
+      </div>
     </>
   );
 }
