25f3086618b8e63e46401b2767559129573e2ead
Mahan-AC <mahanasadpour2005@gmail.com>
2026-07-29 01:10:43 +0330

    Profile Page

---
+++ src/components/Profile/ProfileStats.tsx
@@ -0,0 +1,41 @@
+import { motion } from "framer-motion";
+
+type ProfileStatProps = {
+  title: string;
+  value: number;
+};
+
+export default function ProfileStat({
+  title,
+  value,
+}: ProfileStatProps) {
+  return (
+    <motion.div
+      whileHover={{
+        y: -6,
+        scale: 1.03,
+      }}
+      transition={{
+        duration: 0.2,
+      }}
+      className="
+        rounded-3xl
+        border
+        border-white/40
+        bg-white/30
+        p-6
+        text-center
+        shadow-xl
+        backdrop-blur-xl
+      "
+    >
+      <h2 className="text-4xl font-bold text-[#C47A2C]">
+        {value}
+      </h2>
+
+      <p className="mt-2 text-[#8B5A3C]">
+        {title}
+      </p>
+    </motion.div>
+  );
+}
--- src/core/router.tsx
+++ src/core/router.tsx
@@ -5,6 +5,7 @@ import Register from "../pages/Register";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Welcome from "../pages/Welcome";
+import UserManagement from "../pages/UserManagement";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +21,7 @@ const router = createBrowserRouter([
       //{ path: "home", element: <Home /> },
       //{ path: "refrigerator", element: <Refrigerator />},
       //{ path: "foodList", element: <FoodList />},
-      //{ path: "usermanagement", element: <UserManagement />},
+      { path: "profile/:username", element: <UserManagement />},
     ],
   },
 ]);
